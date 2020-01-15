73c0560bbe4719cd6f9607fa312dbee0a1704261
Sravanthi <sravanthidasari@hotmail.com>
2020-01-15 01:04:33 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,18 +2,27 @@ console.log("Up and running!");
 
 const cards = ["queen", "queen", "king","king"]
 const cardsInPlay = [];
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+ function flipCard(cardId){
+ 	console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
+  checkForMatch();  
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0]=== cardsInPlay[1]){
 		alert ("You found a match!");
 	}
 	else {
 		alert("Sorry, try again.");
-	}
+
+ }
 }
+}
+flipCard(0);
+flipCard(2);
